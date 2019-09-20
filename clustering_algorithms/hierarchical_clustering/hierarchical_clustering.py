079b09223c1eec2fb0fafe114783d350cca42f47
century21ofdev <npng341@gmail.com>
2019-09-21 00:59:13 +0300

    preparing data and model building is completed

--- clustering_algorithms/hierarchical_clustering/hierarchical_clustering.py
+++ clustering_algorithms/hierarchical_clustering/hierarchical_clustering.py
@@ -1,4 +1,14 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.cluster import AgglomerativeClustering
+import matplotlib.pyplot as plt
+
+data_set = pd.read_csv('customers.csv')
+
+X = data_set.iloc[:, 1:].values  # picking age, consumption rate and salary columns and making assignment to X
+
+# Model Building
+
+ac = AgglomerativeClustering(n_clusters=4, affinity='euclidean', linkage='ward')
+y_pred = ac.fit_predict(X)
+# print(y_pred)
 
