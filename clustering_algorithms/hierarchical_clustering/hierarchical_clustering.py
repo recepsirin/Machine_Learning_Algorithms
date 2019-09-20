c75afd852deea22cfdbdd7a30478e51c44142e81
century21ofdev <npng341@gmail.com>
2019-09-21 01:15:38 +0300

    data visualization completed

--- clustering_algorithms/hierarchical_clustering/hierarchical_clustering.py
+++ clustering_algorithms/hierarchical_clustering/hierarchical_clustering.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 data_set = pd.read_csv('customers.csv')
 
-X = data_set.iloc[:, 1:].values  # picking age, consumption rate and salary columns and making assignment to X
+X = data_set.iloc[:, 2:].values  # picking age, consumption rate and salary columns and making assignment to X
 
 # Model Building
 
@@ -19,4 +19,13 @@ ac = AgglomerativeClustering(n_clusters=4, affinity='euclidean', linkage='ward')
 
 y_pred = ac.fit_predict(X)  # making prediction
 
+# Data Visualization
 
+plt.title('Hierarchical Clustering')
+
+plt.scatter(X[y_pred == 0, 0], X[y_pred == 0, 1], s=100, c='purple')
+plt.scatter(X[y_pred == 1, 0], X[y_pred == 1, 1], s=100, c='blue')
+plt.scatter(X[y_pred == 2, 0], X[y_pred == 2, 1], s=100, c='yellow')
+plt.scatter(X[y_pred == 3, 0], X[y_pred == 3, 1], s=100, c='gray')
+
+plt.show()
